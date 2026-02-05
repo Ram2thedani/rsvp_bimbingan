692056f9010f40a7ef3cfab9ea3c0cdf3e95c9ac
Muhamad Ramdani <ram2thedani@gmail.com>
2026-02-05 09:23:01 +0700

    Changed locale to id

--- resources/views/kehadiran/rsvp.blade.php
+++ resources/views/kehadiran/rsvp.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Konfirmasi Kehadiran</title>
+    <title>Konfirmasi Kehadiran - {{ \Carbon\Carbon::parse($sesi->tanggal)->translatedFormat('d F Y') }}</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
